56923b335a5a381d209e88efdcf451db8ca7e8ce
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-09 04:51:23 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars;
+
+chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+];
+
+console.log (chocolateBars);
+  
+
+function addElementToBeginningOfArray(array, element){
+ return [element, ...array]
+}
+
+ function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+ }
+ 
+ function addElementToEndOfArray(array, element){
+  return[...array, element]
+ }
+ 
+ function destructivelyAddElementToEndOfArray(array, element){
+    array.push(element)
+    return array
+ }
+ 
+ function AccessElementInArray (array, index){
+   return array [3]
+ }
+ 
+ function destructivelyRemoveElementFromBeginningOfArray (array){
+  return array.shift()
+ }
